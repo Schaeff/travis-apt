d1752c8e98096ca2a642ec4bf195ebbb20935e43
schaeff <thibaut@schaeff.fr>
2020-01-16 16:41:53 +0100

    first commit

---
+++ ci/before_deploy.sh
@@ -0,0 +1,9 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+
+}
+
+main
---
+++ ci/script.sh
@@ -0,0 +1,10 @@
+# This script takes care of testing your crate
+
+set -ex
+
+# This is the test phase. We will only build as tests happened before.
+main() {
+    cross build --target $TARGET
+}
+
+main
